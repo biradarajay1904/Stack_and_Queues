8b674e4bacd064ba1c01522b68177cbd4bbc361e
biradarajay1904 <biradarajay04@gmail.com>
2022-08-01 16:28:09 +0530

    added: push element to stack

---
+++ Node.java
@@ -0,0 +1,16 @@
+package com.StackandOueues;
+/**
+* ajay biradar
+
+
+*/
+
+public class Node<T> {
+
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+    }
+}
